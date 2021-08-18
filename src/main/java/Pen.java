a5f5c3d4582340717010ecb05bd0b82ab2c3b2ea
David Grieser <david.grieser@galvanize.com>
2021-08-18 13:38:37 -0500

    working click and canWrite methods

--- src/main/java/Pen.java
+++ src/main/java/Pen.java
@@ -1,11 +1,34 @@
 public class Pen {
     int inkLevel;
+    boolean clicked;
 
+    // This is the initial state of the pen
     Pen() {
         inkLevel = 100;
+        clicked = false;
     }
 
     public int inkLeft() {
         return inkLevel;
+    }
+
+    public void click() {
+        // change to opposite
+        // if clicked is true/false
+        if(clicked == true) {
+            clicked = false;
+        } else {
+            clicked = true;
+        }
+    }
+
+    public boolean canWrite() {
+        // if clicked or not
+        if(clicked) {
+            //  return true if clicked open
+            return true;
+        }
+        // return false if clicked closed
+        return false;
     }
 }
